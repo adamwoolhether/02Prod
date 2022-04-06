8e3e54622547dafd869346041d706dd29a71d701
adamwoolhether <adamwoolhether@gmail.com>
2022-04-07 04:48:43 +0800

    Implement TcpListener to use random OS assigned port instead of hardcode

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use std::net::TcpListener;
 use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Bubble up the io::Error if we failed to bind
-    // the address. Otherwise call .await on the server.
-run()?.await
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,34 +1,30 @@
-/// `actix_tr::test` spares us from having to specify `#[test` attribute.
-///
-/// `cargo add actix-rt --dev --version 2` to add `actix-rt` in our Cargo.toml.
-///
-/// Inspect generated code with `cargo expand --test health_check` (<- test file name)
+use std::net::TcpListener;
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Retrieve the port assigned by the OS.
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let _ = tokio::spawn(server);
+    // Return the application addresss to the caller.
+    format!("http://127.0.0.1:{}", port)
+}
 
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
-    // Bring in a request to perform
-    // HTTP requests against our app.
+    let address = spawn_app();
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        // Use the returned app address
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request");
+
+    // Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-/// Without .await, no need fot `spawn_app` to be aysync.
-/// Since we're running tests, no need to propagate errors,
-/// we choose to panic and crash if if fails.
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
-    // Launch server as bkground task,
-    // tokio::spawn returns a handle to the spawned future, but we don't use it here.
-    // so it a non-binding let is used.
-    let _ = tokio::spawn(server);
 }
