7637eefc637e0450d7d8073fbd73273deb13d395
adamwoolhether <adamwoolhether@gmail.com>
2022-04-07 16:04:34 +0800

    DATABASE - Stateful app postgress conn in httpserver. instantiated and pass it in main

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 use std::net::TcpListener;
 use zero2prod::startup::run;
 use zero2prod::configuration::get_configuration;
+use sqlx::{Connection, PgConnection};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Panic if configuration can't be read.
     let configuration = get_configuration().expect("Failed to read configuration");
-    // Remove the hard-coded `8000`, get port from settings.
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -2,12 +2,19 @@ use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer};
 use std::net::TcpListener;
+use sqlx::PgConnection;
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // actix requires our pg conn to be cloneable, we use the `web::Data` extractor,
+    // which wraps our connection in an ARC pointer.
+    let connection = web::Data::new(connection);
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // use actix-web `app_data` method to attach stateful deps,
+            // the cloned connection in this case.
+            .app_data(connection.clone())
     })
         .listen(listener)?
         .run();
