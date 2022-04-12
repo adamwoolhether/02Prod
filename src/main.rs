cab61bd2c8ad59d2bd032130edbd63213ec1d984
adamwoolhether <adamwoolhether@gmail.com>
2022-04-12 16:59:37 +0800

    TRACING - structured loggin implemented

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,31 @@ use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use env_logger::Env;
+use tracing::subscriber::set_global_default;
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // `init` calls `set_logger`, so this is all that's needed.
-    // We'll print all logs at info-level or above if the
-    // `RUST_LOG` env var isn't set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // Fall back to printing all spans at info-level or up
+    // in the RUST_LOG env variable isn't set.
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        // Output the formatted spans to stdout.
+        std::io::stdout
+    );
+    // The `with` method is provided by `SubscriberExt`, and extention
+    // trait for `Subscriber` exposed by `tracing_subscriber`.
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+    // `set_gloabl_default` is used by an app to specify
+    // what subscriber should be used to process spans.
+    set_global_default(subscriber).expect("Failed to set subscriber");
+
     let configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
