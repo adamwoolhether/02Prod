03b6cdade98241cbc78ab0acbd8667cf29ebf966
adamwoolhether <adamwoolhether@gmail.com>
2022-04-14 09:30:30 +0800

    restore actix_web logs with tracing-log crate

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,23 @@ use env_logger::Env;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // Fall back to printing all spans at info-level or up
-    // in the RUST_LOG env variable isn't set.
+    // Redirect all `log`'s events to our subscriber.
+    LogTracer::init().expect("Failed to set logger");
+
     let env_filter = EnvFilter::try_from_default_env()
         .unwrap_or_else(|_| EnvFilter::new("info"));
     let formatting_layer = BunyanFormattingLayer::new(
         "zero2prod".into(),
-        // Output the formatted spans to stdout.
         std::io::stdout
     );
-    // The `with` method is provided by `SubscriberExt`, and extention
-    // trait for `Subscriber` exposed by `tracing_subscriber`.
     let subscriber = Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
         .with(formatting_layer);
-    // `set_gloabl_default` is used by an app to specify
-    // what subscriber should be used to process spans.
     set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration");
