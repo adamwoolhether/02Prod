f628cddd962aeca8bb1a70f876d9f4394570cd13
adamwoolhether <adamwoolhether@gmail.com>
2022-05-12 14:18:49 +0800

    VALIDATION - Required user fields validation func

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,8 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+// An extension trait to use `graphmemes` method on `String` and `&str`
+use unicode_segmentation::UnicodeSegmentation;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -21,6 +23,9 @@ pub struct FormData {
 )]
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    if !is_valid_name(&form.name) {
+        return HttpResponse::BadRequest().finish();
+    }
     match insert_subscriber(&pool, &form).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(e) => {
@@ -28,6 +33,30 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
             HttpResponse::InternalServerError().finish()
         }
     }
+}
+
+// Returns `true` if the input satisfies all validation constrains
+// on subscriber names & `false` otherwise.
+pub fn is_valid_name(s: &str) -> bool {
+    // `.trim()` returns a view over input `s` without
+    // trailing whitespace-like characters.
+    // `.is_empty()` checks if the view contains any character.
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    // A graphmeme is defined by the Unicode standard as a "user-perceived" char.
+    // `å` is a single grapheme, but it is composed of two characters (`a` and `̊`).
+    //
+    // `graphmemes` returns an iterator over the graphememes in input `s`.
+    // `true` specifies that we want to use the extended graphmeme definition set. (recommended)
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    // Iterate over all chars in the input `s` to check if any
+    // match a char in the forbidden array.
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+    // Return `false` if any of our conditions have been violated.
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 }
 
 #[tracing::instrument(
