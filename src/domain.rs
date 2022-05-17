aa1bbc1e85c7d005f858cd115369f1f76c39ca3f
adamwoolhether <adamwoolhether@gmail.com>
2022-05-17 09:26:28 +0800

    VALIDATION - Using claim to provide insightful errors

--- src/domain.rs
+++ src/domain.rs
@@ -5,6 +5,8 @@ pub struct NewSubscriber {
     pub name: SubscriberName,
 }
 
+// Implement debug trait to satisfy our use of `claim`.
+#[derive(Debug)]
 // SubscriberName is a tuple struct with a single, unnamed field of type String.
 pub struct SubscriberName(String);
 
@@ -18,11 +20,11 @@ impl SubscriberName {
         // `.is_empty()` checks if the view contains any character.
         let is_empty_or_whitespace = s.trim().is_empty();
 
-        // A graphmeme is defined by the Unicode standard as a "user-perceived" char.
+        // A grapheme is defined by the Unicode standard as a "user-perceived" char.
         // `å` is a single grapheme, but it is composed of two characters (`a` and `̊`).
         //
-        // `graphmemes` returns an iterator over the graphememes in input `s`.
-        // `true` specifies that we want to use the extended graphmeme definition set. (recommended)
+        // `graphemes` returns an iterator over the graphemes in input `s`.
+        // `true` specifies that we want to use the extended grapheme definition set. (recommended)
         let is_too_long = s.graphemes(true).count() > 256;
 
         // Iterate over all chars in the input `s` to check if any
@@ -31,7 +33,7 @@ impl SubscriberName {
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            panic!("{} is not a valid subscriber name.", s)
+            Err(format!("{} is not a valid subscriber name.", s))
         } else {
             Ok(Self(s))
         }
@@ -41,5 +43,49 @@ impl SubscriberName {
 impl AsRef<str> for SubscriberName {
     fn as_ref(&self) -> &str {
         &self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberName;
+    use claim::{assert_err, assert_ok};
+
+    #[test]
+    fn a_256_grapheme_long_name_is_valid() {
+        let name = "ё".repeat(256);
+        assert_ok!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn a_name_longer_than_256_graphemes_is_rejected() {
+        let name = "a".repeat(257);
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn whitespace_only_names_are_rejected() {
+        let name = " ".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn empty_string_is_rejected() {
+        let name = "".to_string();
+        assert_err!(SubscriberName::parse(name));
+    }
+
+    #[test]
+    fn names_containing_an_invalid_character_are_rejected() {
+        for name in &['/', '(', ')', '"', '<', '>', '\\', '{', '}'] {
+            let name = name.to_string();
+            assert_err!(SubscriberName::parse(name));
+        }
+    }
+
+    #[test]
+    fn a_valid_name_is_parsed_successfully() {
+        let name = "Ursula Le Guin".to_string();
+        assert_ok!(SubscriberName::parse(name));
     }
 }
