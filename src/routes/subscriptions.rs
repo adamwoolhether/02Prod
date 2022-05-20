bb7fec33191ae3538c472ddae043acaa187aec15
adamwoolhether <adamwoolhether@gmail.com>
2022-05-20 11:06:48 +0800

    VALIDATION - Refactoring out parse_subscriber by implementing TryFrom

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,16 @@ pub struct FormData {
     name: String,
 }
 
+impl TryFrom<FormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(value: FormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(value.name)?;
+        let email = SubscriberEmail::parse(value.email)?;
+        Ok(Self { email, name })
+    }
+}
+
 /// This creates a span at the beginning of the function invocation,
 /// attaching all args passed to the function to the span's context.
 #[tracing::instrument(
@@ -21,25 +31,16 @@ pub struct FormData {
         subscriber_name = %form.name
     )
 )]
+
+// subscribe remains in charge of generating HTTP response to incoming request.
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.0.name) {
-        Ok(name) => name,
-        // Return early with a 400 if name is invalid.
+    let new_subscriber = match form.0.try_into() {
+        Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let email = match SubscriberEmail::parse(form.0.email) {
-        Ok(email) => email,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-    // `web::Form` is a wrapper around `FormData`
-    // `form.0` gives access to the underlying `FormData`
-    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
-        Err(e) => {
-            tracing::error!("Failed to execute query: {:?}", e);
-            HttpResponse::InternalServerError().finish()
-        }
+        Err(_) => HttpResponse::InternalServerError().finish(),
     }
 }
 
