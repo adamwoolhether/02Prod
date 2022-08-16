1f1793be8b7f182e11e23bc039df9e3e25f311b4
adamwoolhether <adamwoolhether@gmail.com>
2022-08-16 14:57:09 +0800

    implement wiremock for testing

--- src/email_client.rs
+++ src/email_client.rs
@@ -23,6 +23,45 @@ impl EmailClient {
         html_content: &str,
         text_content: &str,
     ) -> Result<(), String> {
-        todo!()
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::domain::SubscriberEmail;
+    use crate::email_client::EmailClient;
+    use fake::faker::internet::en::SafeEmail;
+    use fake::faker::lorem::en::{Paragraph, Sentence};
+    use fake::{Fake, Faker};
+    use wiremock::matchers::any;
+    use wiremock::{Mock, MockServer, ResponseTemplate};
+
+    #[tokio::test]
+    async fn send_email_fires_a_request_to_base_url() {
+        // Arrange
+
+        // Start the mock HTTP server
+        let mock_server = MockServer::start().await;
+        let sender = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let email_client = EmailClient::new(mock_server.uri(), sender); // Point address of mock server to email client.
+
+        Mock::given(any()) // Specify matching conditions of the mock server with any().
+            .respond_with(ResponseTemplate::new(200))
+            .expect(1) // Set an expectation on the mock: that the test should receive only one request.
+            .mount(&mock_server)
+            .await;
+
+        // Run the EmailClient. fake() helps general random data to send inputs to send_mail.
+        let subscriber_email = SubscriberEmail::parse(SafeEmail().fake()).unwrap();
+        let subject: String = Sentence(1..2).fake();
+        let content: String = Paragraph(1..10).fake();
+
+        //Act
+        let _ = email_client
+            .send_email(subscriber_email, &subject, &content, &content)
+            .await;
+
+        // Assert
     }
 }
