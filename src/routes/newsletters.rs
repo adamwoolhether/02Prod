fbda8b3d9ed3af6d1864209b47a04088aa8ce68e
adamwoolhether <adamwoolhether@gmail.com>
2023-02-02 21:08:59 +0800

    add test and implemented dummy publish_newsletter handler

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,5 +1,19 @@
+use actix_web::web;
 use actix_web::HttpResponse;
 
-pub async fn publish_newsletter() -> HttpResponse {
+#[derive(serde::Deserialize)]
+pub struct BodyData {
+    title: String,
+    content: Content,
+}
+
+#[derive(serde::Deserialize)]
+pub struct Content {
+    html: String,
+    text: String,
+}
+
+// Prefixing `body` with a `_` to avoid compiler warning about unused args
+pub async fn publish_newsletter(_body: web::Json<BodyData>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
